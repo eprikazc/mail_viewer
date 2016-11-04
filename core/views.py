d917e47e3f4dc5b05f1c82306c6221343a007361
Eugene Prikazchikov <eprikazc@gmail.com>
2016-11-04 13:58:55 +0300

    Convert view to use APIView from DRF

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from apiclient import discovery
 from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from core.google_api_utils import APIClient, parse_email
 
@@ -9,23 +10,27 @@ def home(request):
     return render(request, 'core/home.html')
 
 
-def email(request):
-    emails_to_retrieve = 100
-    emails = {}
+class EmailList(APIView):  # using DRF is a requirement
 
-    def email_fetched(request_id, response, exception):
-        emails[response['id']] = parse_email(response)
+    def get(self):
+        emails_list = self.batch_retrieve_emails(100)
+        return Response({'messages': emails_list})
 
-    api_client = APIClient(request.user)
-    service = discovery.build('gmail', 'v1')
-    res = api_client.execute(
-        service.users().messages().list(userId='me', maxResults=emails_to_retrieve))
-    ordered_message_ids = []
-    batch = service.new_batch_http_request(callback=email_fetched)
-    for message in res['messages']:
-        batch.add(
-            service.users().messages().get(userId='me', id=message['id']))
-        ordered_message_ids.append(message['id'])
-    api_client.execute(batch)
-    emails_list = [emails[msg_id] for msg_id in ordered_message_ids]
-    return JsonResponse({'messages': emails_list})
+    def batch_retrieve_emails(self, emails_to_retrieve):
+        emails = {}
+        ordered_message_ids = []
+
+        def email_fetched(request_id, response, exception):
+            emails[response['id']] = parse_email(response)
+
+        api_client = APIClient(self.request.user)
+        service = discovery.build('gmail', 'v1')
+        res = api_client.execute(
+            service.users().messages().list(userId='me', maxResults=emails_to_retrieve))
+        batch = service.new_batch_http_request(callback=email_fetched)
+        for message in res['messages']:
+            batch.add(
+                service.users().messages().get(userId='me', id=message['id']))
+            ordered_message_ids.append(message['id'])
+        api_client.execute(batch)
+        return [emails[msg_id] for msg_id in ordered_message_ids]
