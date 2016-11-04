e0d6302c34d14cd2285aecfc3c2920b7ff9598a1
Eugene Prikazchikov <eprikazc@gmail.com>
2016-11-04 15:30:12 +0300

    Use Angular routing

--- core/static/core/js/app.js
+++ core/static/core/js/app.js
@@ -1,9 +1,20 @@
-var gmailViewerModule = angular.module('GmailViewer', []);
-gmailViewerModule.controller('MainController', function($scope, $http) {
-  $scope.messages = [];
-  $http.get("/email/")
-  .then(function(response) {
-    $scope.messages = response.data.messages;
-    console.log($scope.messages);
+(function () {
+  "use strict";
+
+  var gmailViewerModule = angular.module('GmailViewer', ['ngRoute']);
+  gmailViewerModule.controller('MainController', function($scope, $http) {
+    $scope.messages = [];
+    $http.get("/email/")
+    .then(function(response) {
+      $scope.messages = response.data.messages;
+    });
   });
-});
+
+  gmailViewerModule.config(function($routeProvider) {
+    $routeProvider
+     .when('/', {
+      templateUrl: '/static/core/templates/messages.html',
+      controller: 'MainController',
+    });
+  });
+} ());
